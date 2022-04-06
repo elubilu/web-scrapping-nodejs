30fe4c929827886b07526cb5bfec500a48e21960
Rasel Ahmed <raselahmed.lucse@gmail.com>
2022-04-06 01:37:12 +0100

    Bypass browser access to get access to the webpage when it's detect bot

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const puppeteer = require("puppeteer")
-// const fs = require("fs/promises")
 
-async function start() {
-  const browser = await puppeteer.launch()
+
+// puppeteer-extra is a drop-in replacement for puppeteer,
+// it augments the installed puppeteer with plugin functionality
+const puppeteer = require('puppeteer-extra')
+
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+// puppeteer usage as normal
+puppeteer.launch({ headless: true }).then(async browser => {
+  console.log('Running tests..')
   const page = await browser.newPage()
-  await page.goto("https://www.lens.org")
-
-  //Take screenshot of a webpage
-  await page.screenshot({path : "test.png"})
-
-  //Take screenshot of a full webpage
-  await page.screenshot({path : "test.png", fullPage: true})
-
+  await page.goto('https://www.lens.org/lens/search/patent/list?q=reference_cited.patent.lens_id:134-208-029-138-815')
+  await page.waitForTimeout(5000)
+  await page.screenshot({ path: 'testResult.png', fullPage: true })
   await browser.close()
-}
-
-start()
+  console.log(`All done, check the screenshot. ✨`)
+})
