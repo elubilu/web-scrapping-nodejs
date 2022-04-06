9540acc16c203fca0fd0f0333efdefae59a90c83
Rasel Ahmed <raselahmed.lucse@gmail.com>
2022-04-06 01:15:29 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const puppeteer = require("puppeteer")
+// const fs = require("fs/promises")
+
+async function start() {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto("https://www.lens.org")
+  await page.screenshot({path : "test.png"})
+  await browser.close()
+}
+
+start()
