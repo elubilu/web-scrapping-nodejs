a6c13c2429da3c9612860489c3d8cd71bc5f5a80
Rasel Ahmed <raselahmed.lucse@gmail.com>
2022-04-21 15:25:18 +0100

    Multiple data collection by link

--- index.js
+++ index.js
@@ -1,33 +1,75 @@
 const puppeteer = require('puppeteer-extra')
+const csvParse = require('csv-parse')
+const csvParser = require('csv-parser')
 const fs =  require('fs.promises')
+const fsC =  require('fs')
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
+
+
 // puppeteer usage as normal
-puppeteer.launch({ headless: true }).then(async browser => {
+puppeteer.launch({ headless: false }).then(async browser => {
     console.log('Running tests..')
     try {
-        const page = await browser.newPage()
-        await page.goto('https://www.lens.org/lens/search/patent/list?q=reference_cited.patent.lens_id:134-208-029-138-815')
-        await page.waitForTimeout(5000)
-        await page.screenshot({ path: new Date().getTime().toString()+".png", fullPage: true })
-        console.log("Initial Screenshot Taken...")
-        console.log("Clicking to Export....")
-        await page.waitForTimeout(5000)
-        await Promise.all([page.click(".analysis-toolbar-item") ])
-        await page.screenshot({ path: "hello.png", fullPage: true })
-        console.log("Second Screenshot Taken...")
+        fsC.createReadStream("patentList/ListOfPatent2.csv")
+        .pipe(csvParser())
+        .on('data', async function(data){
+            try {
+                console.log("Link is: "+data.CitedLink);
+                console.log("Patent Number is: "+data.PatentNo);
+                // var url = 'https://www.lens.org/lens/search/patent/list?q=reference_cited.patent.lens_id:'+data.LensId;
+                const page = await browser.newPage();
+                await page.goto(data.CitedLink);
+                await page.waitForSelector('.btn.btn-xs.analysis-toolbar-item.ng-binding');
+                await page.evaluate(() => {
+                    document.querySelectorAll(".btn.btn-xs.analysis-toolbar-item.ng-binding")[3].click();
+                });
+                await page.waitForSelector('[name="filename"]');
+                await page.evaluate(() => {
+                    document.querySelector('[name="filename"]').value = data.PatentNo;
+                });
+                await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './myAwesomeDownloadFolder'});
+                await page.waitForSelector('.btn.btn-sm.btn-pri.ng-binding');
+                await page.evaluate(() => {
+                    document.querySelector(".btn.btn-sm.btn-pri.ng-binding").click();
+                });
+                // await page.waitFor(Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000)
+            }
+            catch(err) {
+                //error handler
+            }
+        })
+        .on('end',async function(){
+            //some final operation
+                // const page = await browser.newPage();
+                // await page.goto("https://www.lens.org/lens/search/patent/list?q=reference_cited.patent.lens_id:068-887-823-650-053");
+                // await page.waitFor(5000);
+                // await page.waitForSelector('.btn.btn-xs.analysis-toolbar-item.ng-binding');
+                // await page.evaluate(() => {
+                //     document.querySelectorAll(".btn.btn-xs.analysis-toolbar-item.ng-binding")[3].click();
+                // });
+                // await page.waitForSelector('[name="filename"]');
+                // await page.evaluate(() => {
+                //     document.querySelector('[name="filename"]').value = data.PatentNo;
+                // });
+                // await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './myAwesomeDownloadFolder'});
+                // await page.waitForSelector('.btn.btn-sm.btn-pri.ng-binding');
+                // await page.evaluate(() => {
+                //     document.querySelector(".btn.btn-sm.btn-pri.ng-binding").click();
+                // });
+                // await page.waitFor(Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000)
+        });  
         
-        console.log("Clicking to Export Final....")
-        await page.waitForTimeout(5000)
-        await page.click(".btn-pri");
-        
+       
+        // await page.screenshot({ path: new Date().getTime().toString()+".png"});
+        console.log('Ended test..')
     } catch (e) {
         console.log(e)
     }
 
 
 
-  await browser.close()
-  console.log(`All done, check the screenshot. ✨`)
+//   await browser.close()
+  console.log(`All done, check the file ✨`)
 })
